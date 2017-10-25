e91c036c76938d66d83c01479641837bd05e0f2c
Marek Foltýn <marek@foltynovi.cz>
2017-10-25 12:34:27 +0200

    std::experimental::filesystem working on windows.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@
 // created: 25.10.2017.
 //
 
+#include <filesystem>
+#include <iostream>
+
 int main(int argc, char * argv[])
 {
-
+    std::cout << "Hi bro:" << std::experimental::filesystem::current_path() << std::endl;
 }
